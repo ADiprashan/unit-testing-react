689238e61548861bdf4ce99f09da6c91d8e5c3d7
Rajitha <rajithapd@gmail.com>
2020-02-16 12:35:14 +1000

    Headline component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Header from "./comoponets/header";
+import Headline from "./comoponets/headline";
+import "./app.scss";
 
 function App() {
   return (
     <div>
       <Header />
+      <section className="main">
+        <Headline
+          header="Posts"
+          desc="Click the button to render posts!"
+          tempArr={tempArr}
+        />
+      </section>
     </div>
   );
 }
